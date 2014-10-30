0eb3df068fc98e8f5fe58d79da5b95683fadf828
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2014-10-30 12:07:49 +0100

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='edir-reminder-service',
+	description='eDirectory Login/Password Email Reminder Service',
+    author='Dr. Stefan Schimanski',
+    author_email='stefan.schimanski@gmail.com',
+    url='https://github.com/sttts/edir-reminder-service',
+    packages=['edir_reminder_service'],
+    #data_files=[
+    #	('/etc/edir-reminder-service', ['login.conf', 'password.conf']),
+    #],
+    package_data = {'edir_autoyast_ws': ['templates/*', 'static/*/*']},
+    py_modules = ['edir_reminder_service_main']
+)
