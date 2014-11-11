637092b2ab192b1f0962a1a65f6d49926c48937e
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2014-11-11 15:05:10 +0100

    Fix typos in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='ldap-notify',
 	description='LDAP Login/Password Email Notification Service',
-	version='0.8.1',
+	version='0.8.2',
 	long_description="""\
       `ldap-notify` searches users in an LDAP directory whose login or password expires in a certain number of days. These users are notified with a customizable email. The time intervals for the notifications and the email templates can be freely defined. By using a custom LDAP attribute the last notification is stored in the directory such that users are only notified once per rule. Moreover, this mechanism allows running the tool (e.g. via cron) on multiple machine with a certain time delta in order to implement high availabiliy. 
       """,
@@ -12,7 +12,7 @@ setup(name='ldap-notify',
     author_email='stefan.schimanski@gmail.com',
     url='https://github.com/sttts/ldap_notify',
     packages=['ldap_notify'],
-    package_data = {'ldap-notify': ['templates/*']},
+    package_data = {'ldap_notify': ['templates/*']},
     scripts=[ 'scripts/ldap-notify' ],
 	classifiers=[
 		"License :: OSI Approved :: GNU General Public License (GPL)",
@@ -23,6 +23,6 @@ setup(name='ldap-notify',
 	keywords='ldap notification eDirectory',
 	license='GPL',
 	install_requires=[
-		'ldap',
+		'python-ldap',
 	]
 )
