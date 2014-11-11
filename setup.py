fe01fef66043f5434594d65278b1931f2f495330
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2014-11-11 13:09:18 +0100

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='ldap_notify',
 	description='LDAP Login/Password Email Notification Service',
+	version='0.8.0',
+	long_description="""\
+      `ldap-notify` searches users in an LDAP directory whose login or password expires in a certain number of days. These users are notified with a customizable email. The time intervals for the notifications and the email templates can be freely defined. By using a custom LDAP attribute the last notification is stored in the directory such that users are only notified once per rule. Moreover, this mechanism allows running the tool (e.g. via cron) on multiple machine with a certain time delta in order to implement high availabiliy. 
+      """,
     author='Dr. Stefan Schimanski',
     author_email='stefan.schimanski@gmail.com',
     url='https://github.com/sttts/ldap_notify',
     packages=['ldap_notify'],
-    data_files=[
-    	('/etc/ldap-notify', ['conf/login.conf', 'conf/password.conf']),
-    ],
     package_data = {'ldap-notify': ['templates/*']},
-    scripts = [ 'scripts/ldap-notify' ]
+    scripts=[ 'scripts/ldap-notify' ],
+	classifiers=[
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+	  	"Programming Language :: Python",
+	  	"Development Status :: 4 - Beta",
+	  	"Intended Audience :: Administrators",
+	  	"Topic :: LDAP",
+	],
+	keywords='ldap notification eDirectory',
+	license='GPL',
+	install_requires=[
+		'ldap',
+	]
 )
